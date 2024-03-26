7bfa6f6a95311656b5e30ab8fded147681878642
Eajun Ooi <eajunooi@gmail.com>
2024-03-26 09:28:32 +0800

    Add database connection

--- Stock/LLM.py
+++ Stock/LLM.py
@@ -2,12 +2,25 @@ import logging
 from config import Config
 import llama
 import torch
+from llama import Client
+from pymongo import MongoClient
 
 # Set CUDA device if available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+connection_string = "mongodb+srv://eajun:eajun030802@cluster0.ktfe47o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Initialize the MongoClient with your connection string
+client = MongoClient(connection_string)
+
+#FIXME: Cannot get the text for ..% keywords
+db = client.Eajun
+collection = db["textData"]
+
+# Create a text index on the "keywords" field
+collection.create_index([("keywords", "text")])
+
 #TODO: The latency is too HIGH(Change to GPU)
-#FIXME: The words are not coming out when generating
 def LLM():
     # Set the CUDA device for PyTorch
     if torch.cuda.is_available():
@@ -23,10 +36,9 @@ def LLM():
             Always introduce yourself first. 
             Behave yourself like a capybara but still answer like a Llama.
             You are a stock advisor for United State stock market.
-            You should provide your own predictions on the future stock market.
-            You should provide more information on the stock market.
-            Provide short answers but enough information
-            
+            You should provide your own predictions on the future stock market if the question related to stock.
+            You should provide more information on the stock market if the question related to stock.
+            Provide short answers but enough information.
         """.strip().replace("\n", " ")
 
         logging.info("System message: %s", systemMessage)
@@ -34,18 +46,26 @@ def LLM():
 
         # Llama generate response here using the loaded model
         try:
-            response = llama.chat(model=Config.ollamaModelName, messages=[
-                {
-                    'role': 'system',
-                    'content': systemMessage,
-                },
-                {
-                    'role': 'system',
-                    'content': message,
-                },
-            ])
-            response_message = response['message']['content']
-            print("Capybara:" + response_message)
+            # client = Client(host='http://172.22.45.57:11434')
+            stream = llama.chat(
+                model='llama2:13b',
+                messages=[
+                    {
+                        'role': 'system',
+                        'content': systemMessage,
+                    },
+                    {
+                        'role': 'system',
+                        'content': message,
+                    },
+                ],
+                stream=True,
+            )
+            # response_message = response['message']['content']
+            print("CapybaraAI: ")
+            for chunk in stream:    
+                print(chunk['message']['content'], end='', flush=True)
+            print("\n")
 
         except Exception:
             logging.error("Failed to generate response for message: %s", message, exc_info=True)
